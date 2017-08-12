dabc03f4e95055562da1357e89ebf33998bb1629
wintolearn <jmartin@wintolearn.com>
2017-08-12 10:40:52 -0400

    Initial Commit

---
+++ Starcoder ios 1.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Starcoder ios 1.1
+//
+//  Created by Jonathan Martin on 8/12/17.
+//  Copyright © 2017 Jonathan Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
